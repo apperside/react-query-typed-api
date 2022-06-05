79b2a52cca12ad3eb4644193a2637bb5cd8170a0
Simone G <gaspari.simone@gmail.com>
2022-06-05 15:23:13 +0200

    fix AppQuery types

--- src/components/AppQuery.ts
+++ src/components/AppQuery.ts
@@ -2,12 +2,12 @@ import { UseQueryResult, UseQueryOptions } from "react-query"
 import { AppRoutes, ApiResponseType, AppQueryOptions, useAppQuery } from ".."
 
 type AppQueryProps<Scope extends keyof AppRoutes = "main", T extends keyof AppRoutes[Scope] = keyof AppRoutes[Scope]> = {
-	children: (query: UseQueryResult<ApiResponseType<Scope, T>>) => JSX.Element | null
+	children: (query: UseQueryResult<ApiResponseType<Scope, T>>) => JSX.Element | JSX.Element[] | undefined | null
 	routeOrRouteObj: T | { scope: Scope, route: T },
 	appQueryOptions?: Partial<Omit<AppQueryOptions, "payload" | "apiScope">>,
 	useQueryOptions?: UseQueryOptions
 }
-export function AppQuery<Scope extends keyof AppRoutes = "main", T extends keyof AppRoutes[Scope] = keyof AppRoutes[Scope]>(props: AppQueryProps<Scope, T>): JSX.Element | null {
+export function AppQuery<Scope extends keyof AppRoutes = "main", T extends keyof AppRoutes[Scope] = keyof AppRoutes[Scope]>(props: AppQueryProps<Scope, T>): JSX.Element | JSX.Element[] | undefined | null {
 
 	const query = useAppQuery(props.routeOrRouteObj, props.appQueryOptions, props.useQueryOptions)
 	return props.children(query)

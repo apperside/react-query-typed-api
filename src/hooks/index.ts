58de54e8d72bd84dc8ee59dea127921ca6829f43
Apperside <info@apperside.com>
2022-09-25 18:01:13 +0200

    fix typings

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,12 @@
-import { HttpRequestOptions } from "src/networking"
+// import { HttpRequestOptions } from "src/networking"
 
-export { useAppMutation } from "./useAppMutation"
-export { useAppQuery } from "./useAppQuery"
-export * from "./useAppQueryClient"
-export * from "./useAppTask"
+import { HttpRequestOptions } from "..";
 
+export { useAppMutation } from "./useAppMutation";
+export { useAppQuery } from "./useAppQuery";
+export * from "./useAppQueryClient";
+export * from "./useAppTask";
 
 export type AppQueryOptions<Payload = any> = {
-	pathParams?: { [key: string]: any }
-} & Omit<HttpRequestOptions<Payload>, "url">
+  pathParams?: { [key: string]: any };
+} & Omit<HttpRequestOptions<Payload>, "url">;
